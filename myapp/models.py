299a38d70148af0ff948af2e5b0a5e8c401a79ed
artem <vonap88@mail.ru>
2023-06-06 21:56:55 +0300

    сщздание таблиц

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    age = models.IntegerField(verbose_name='возраст')
+    city = models.CharField(max_length=100, verbose_name='Город')
+    is_activated = models.BooleanField(verbose_name='активация')
+
+
+    class Meta:
+        verbose_name = 'Человек'
+        verbose_name_plural = 'Люди'
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=30, verbose_name='бренд')
+    model = models.CharField(max_length=30, verbose_name='модель')
+    color = models.CharField(max_length=30, verbose_name='цвет')
+    power = models.IntegerField(verbose_name='мощность')
+    year = models.IntegerField(verbose_name='год производства')
+
+    def __str__(self):
+        return self.brand
+
+
+    class Meta:
+        verbose_name = 'Автомобиль'
+        verbose_name_plural = 'Машины'
--- myapp/views.py
+++ myapp/views.py
@@ -7,6 +7,17 @@ def about(request):
 def login(request):
     return HttpResponse('page login')
 
-def contacts(request):
-    return HttpResponse('page contacts')
+def contacts(request,id):
+    url_id = id
+    name = request.GET.get('name')
+    age = request.GET.get('age')
+    get_params = {'name':name, 'age': age}
+    return HttpResponse(f'page contacts, url_paramttr_id = {url_id}, get_params - {get_params}')
+
+
+def index_myapp(request):
+    return HttpResponse('myapp page')
+
+def index(request):
+    return HttpResponse('main page')
 
